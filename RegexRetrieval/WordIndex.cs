90e34acfaac807c26bbcb8a1e0fef11ad0f6eb4f
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-02-07 15:43:52 +0100

    Faster WordIndex

--- RegexRetrieval/WordIndex.cs
+++ RegexRetrieval/WordIndex.cs
@@ -5,38 +5,48 @@ namespace RegexRetrieval
 {
     public class WordIndex
     {
-        // TODO: variable length
-        private const int LengthBitMask = 0xFFFFF;
-
         private readonly string[] words;
         private readonly int[][] index;
+        private readonly int bitMask;
 
         public WordIndex(string[] words)
         {
             this.words = words;
-            this.index = CreateIndex(words);
+            bitMask = NextPowerOfTwo(words.Length) / 2 - 1;
+            index = CreateIndex(words, bitMask);
         }
 
-        private static int[][] CreateIndex(string[] words)
+        private static int NextPowerOfTwo(int n)
         {
-            var expectedLength = words.Length / (LengthBitMask + 1);
-            expectedLength += expectedLength >> 1;
-            var lists = new List<int>[LengthBitMask + 1].Set(i => new List<int>(expectedLength));
+            n--;
+            n |= n >> 1;
+            n |= n >> 2;
+            n |= n >> 4;
+            n |= n >> 8;
+            n |= n >> 16;
+            n++;
+            return n;
+        }
+
+        private static int[][] CreateIndex(string[] words, int bitMask)
+        {
+            var expectedLength = words.Length / (bitMask + 1);
+            expectedLength += expectedLength >> 1 + 1;
+            var lists = new List<int>[bitMask + 1].Set(i => new List<int>(expectedLength));
 
             for (int i = 0; i < words.Length; i++)
             {
                 int hash = Hash(words[i]);
-                lists[hash & LengthBitMask].Add(i);
+                lists[hash & bitMask].Add(i);
             }
 
             var index = new int[lists.Length][];
             for (int i = 0; i < index.Length; i++)
             {
-                var binaryIndex = lists[i].ToArray();
-                lists[i] = null;
-                Array.Sort(binaryIndex, (a, b) => String.CompareOrdinal(words[a], words[b]));
-                if (binaryIndex.Length > 0)
-                    index[i] = binaryIndex;
+                ref var list = ref lists[i];
+                if (list.Count > 0)
+                    index[i] = list.ToArray();
+                list = null;
             }
 
             return index;
@@ -57,28 +67,19 @@ namespace RegexRetrieval
             return i;
         }
 
-        private int BinarySearch(int[] binaryIndex, string word)
-        {
-            int low = 0;
-            int high = binaryIndex.Length;
-
-            while (low < high)
-            {
-                var middle = low + ((high - low) >> 1);
-                var c = String.CompareOrdinal(word, words[binaryIndex[middle]]);
-                if (c == 0) return binaryIndex[middle];
-                if (c > 0) low = middle + 1;
-                else high = middle;
-            }
-            return -1;
-        }
-
         public int GetIndex(string word)
         {
             var hash = Hash(word);
-            var binaryIndex = index[hash & LengthBitMask];
-            if (binaryIndex == null) return -1;
-            return BinarySearch(binaryIndex, word);
+            var wordIndex = index[hash & bitMask];
+            if (wordIndex == null) return -1;
+
+            foreach (var i in wordIndex)
+            {
+                var iWord = words[i];
+                if (iWord.Equals(word, StringComparison.Ordinal))
+                    return i;
+            }
+            return -1;
         }
 
     }
